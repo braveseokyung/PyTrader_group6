9343603df598769f11ab215e6528ce256c4b835a
Jinyong Eom <wlsdyd7511@gmail.com>
2022-08-27 22:34:50 +0900

    19-1-2

--- pymon.py
+++ pymon.py
@@ -3,7 +3,9 @@ from PyQt5.QtWidgets import *
 import Kiwoom
 import datetime
 import webreader
+import time
 import numpy as np
+from pandas import Series, DataFrame
 
 MARKET_KOSPI = 0
 MARKET_KOSDAQ = 10
@@ -16,13 +18,27 @@ class PyMon:
 
     # noinspection PyMethodMayBeStatic
     def run(self):
-        print(self.kospi_codes)
-        print(self.kosdaq_codes)
+        df = self.get_ohlcv('039490', '20220701')
+        print(df)
 
     # noinspection PyMethodMayBeStatic
     def get_code_list(self):
         self.kospi_codes = self.kiwoom.get_code_list_by_market(MARKET_KOSPI)
         self.kosdaq_codes = self.kiwoom.get_code_list_by_market(MARKET_KOSDAQ)
+
+    def get_ohlcv(self, code, start):
+        self.kiwoom.ohlcv = {'date': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': []}
+
+        self.kiwoom.set_input_value('종목코드', code)
+        self.kiwoom.set_input_value('기준일자', start)
+        self.kiwoom.set_input_value('수정주가구분', 1)
+        self.kiwoom.comm_rq_data('opt10081_req', 'opt10081', 0, '0101')
+        time.sleep(0.2)
+
+        df = DataFrame(self.kiwoom.ohlcv, columns=['open', 'high', 'low', 'close', 'volume'],
+                       index=self.kiwoom.ohlcv['date'])
+
+        return df
 
     # noinspection PyMethodMayBeStatic
     def calculate_estimated_dividend_to_treasury(self, code):
